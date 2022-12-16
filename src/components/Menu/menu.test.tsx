67923013755d3321c4ab8172123147a55f745448
Vita <dragic.hsieh@gmail.com>
2022-12-16 17:11:41 +0800

    add basic SubMenu test code

--- src/components/Menu/menu.test.tsx
+++ src/components/Menu/menu.test.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { cleanup, fireEvent, render, RenderResult } from '@testing-library/react';
+import { cleanup, fireEvent, render, RenderResult, waitFor } from '@testing-library/react';
 
 import Menu, { MenuProps } from './Menu';
 import MenuItem from './MenuItem';
+import SubMenu from './SubMenu';
 
 const testProps: MenuProps = {
 	defaultIndex: '0',
@@ -21,8 +22,27 @@ const GenerateMenu = (props: MenuProps) => {
 			<MenuItem>active</MenuItem>
 			<MenuItem disabled>disabled</MenuItem>
 			<MenuItem>third item</MenuItem>
+			<SubMenu title='dropdown'>
+				<MenuItem>dropdown 1</MenuItem>
+			</SubMenu>
 		</Menu>
 	);
+};
+
+const createStyleFile = () => {
+	const cssFile = `
+		.viking-submenu {
+			display: none;
+		}
+		.viking-submenu.menu-opened {
+			display: block;
+		}
+	`;
+	const style = document.createElement('style');
+	style.type = 'text/css';
+	style.innerHTML = cssFile;
+
+	return style;
 };
 
 let wrapper: RenderResult,
@@ -34,6 +54,7 @@ let wrapper: RenderResult,
 describe('test Menu Component', () => {
 	beforeEach(() => {
 		wrapper = render(GenerateMenu(testProps));
+		wrapper.container.append(createStyleFile());
 		menuElement = wrapper.getByTestId('test-menu');
 		activeElement = wrapper.getByText('active');
 		disabledElement = wrapper.getByText('disabled');
@@ -42,7 +63,7 @@ describe('test Menu Component', () => {
 	it('should render correct Menu and MenuItem based on default props', () => {
 		expect(menuElement).toBeInTheDocument();
 		expect(menuElement).toHaveClass('viking-menu test');
-		expect(menuElement.getElementsByTagName('li').length).toEqual(3);
+		expect(menuElement.querySelectorAll(':scope > li').length).toEqual(4);
 		expect(activeElement).toHaveClass('menu-item is-active');
 		expect(disabledElement).toHaveClass('menu-item is-disabled');
 	});
@@ -68,5 +89,24 @@ describe('test Menu Component', () => {
 		const menuElement = wrapper.getByTestId('test-menu');
 
 		expect(menuElement).toHaveClass('menu-vertical');
+	});
+
+	it('should show dropdown items when hover on subMenu', async () => {
+		expect(wrapper.queryByText('dropdown 1')).not.toBeVisible();
+
+		const dropdownElement = wrapper.getByText('dropdown');
+
+		fireEvent.mouseEnter(dropdownElement);
+		await waitFor(() => {
+			expect(wrapper.queryByText('dropdown 1')).toBeVisible();
+		});
+
+		fireEvent.click(wrapper.getByText('dropdown 1'));
+		expect(testProps.onSelect).toHaveBeenCalledWith('3-0');
+
+		fireEvent.mouseLeave(dropdownElement);
+		await waitFor(() => {
+			expect(wrapper.queryByText('dropdown 1')).not.toBeVisible();
+		});
 	});
 });
